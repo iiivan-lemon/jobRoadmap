8018ee10d1f2ffc6ef19fce60f3215ae9b2f0113
Ivan <lipton2807@yandex.ru>
2023-04-14 18:16:06 +0300

    new(jobLetter): add front mock

--- src/pages/jobLetterPage/jobLetterPage.tsx
+++ src/pages/jobLetterPage/jobLetterPage.tsx
@@ -3,6 +3,8 @@ import { loadState } from '../../utils/utils'
 import { getResResume } from '../../models/resume/resumeFixSlice'
 import { useAppDispatch } from '../../app/hooks'
 import './jobLetter.css'
+import { getJobLetter } from '../../models/jobLetter/jobLetterSlice'
+import { PushSpinner } from 'react-spinners-kit'
 export const JobLetterPage = () => {
   const dispatch = useAppDispatch()
   const [
@@ -12,34 +14,46 @@ export const JobLetterPage = () => {
   const [selectedFile, setSelectedFile] = React.useState(null)
 
   const [selectedUrl, setSelectedUrl] = React.useState(null)
-
+  const [data, setData] = React.useState(null)
   const handleFileSelect = (event) => {
+    setLoad(loadState.base)
     setSelectedFile(event.target.files[0])
   }
   const handleUrlSelect = (event) => {
+    setLoad(loadState.base)
     setSelectedUrl(event.target.value)
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault()
     // eslint-disable-next-line no-debugger
+    debugger
+    if (!(selectedFile && selectedUrl)) {
+      return
+    }
+
     const formData = new FormData()
-    // @ts-expect-error qwer
     formData.append('file', selectedFile)
     try {
       setLoad(loadState.load)
-      void dispatch(getResResume(formData.get('file')))
+      void dispatch(getJobLetter({ resume: formData.get('file'), url: selectedUrl }))
         .then(data => {
           if (!data.payload) {
             setLoad(loadState.error)
           } else {
             setLoad(loadState.res)
-            // setData(data.payload)
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            setData(data.payload)
           }
         }).catch(() => { setLoad(loadState.error) })
     } catch (error) {
       setLoad(loadState.error)
     }
+  }
+
+  const renderLetter = (text) => {
+    return text
   }
 
   return (
@@ -49,10 +63,22 @@ export const JobLetterPage = () => {
         <form onSubmit={handleSubmit}>
           <input type="file" onChange={handleFileSelect}/>
           <input type="text" onChange={handleUrlSelect}/>
+          <input type="submit" value="получить сопроводительное письмо" />
         </form>
       </div>
+      <div className='preloader'>
+        <PushSpinner
+          color="#686769"
+          id="preloader"
+          loading={loading === loadState.load}
+          size={30}
+        />
+      </div>
+      {(loading === loadState.res) &&
+          <>
+              <textarea>{renderLetter(data)}</textarea></>
+      }
 
-      <textarea>письмо</textarea>
     </div>
   )
 }
